7ed72d2f76c94c24c9d2284c4ca111f5835d1db6
Thief3 <kissarlim@gmail.com>
2021-01-15 21:08:56 +0000

    Table now displays the correct tiles and images. Working on tile data now.

--- reboot/src/assets/scripts/tiles.js
+++ reboot/src/assets/scripts/tiles.js
@@ -1,61 +1,82 @@
 'use strict;'
 
+var res    = {original: [], current_filtered: []};
+var locale = {};
+var cats   = {};
+var tiles  = {};
+
 // Call create table each time this is modified
-var res = {original: [], current_filtered: []};
+async function Setup() {
+    locale = await GetJson("https://raw.githubusercontent.com/JROB774/tein-manual/master/reboot/src/reference/en.json");
+    cats   = await GetJson("https://raw.githubusercontent.com/JROB774/tein-manual/master/reboot/src/reference/categories.json");
+    tiles  = await GetJson("https://raw.githubusercontent.com/JROB774/tein-manual/master/reboot/src/reference/tiles.json");
 
-/*
-Papa.parse("./tiles.csv", {
-    download: true,
-    delimiter: "|",
-    header: true,
-	complete: function(results) {
-        console.log(results);
-        res.original = results.data;
-        res.current_filtered = res.original
-        createTable(res.current_filtered, 10);    
+    console.log(cats);
+
+    for(var cat in cats){
+        console.log(cat);
+        createTable(cats[cat].tiles, 10, cats[cat].name);
     }
-});
-*/
-
-//var locale = JSON.parse("./en.json");
-//var cats = JSON.parse("./categories.json");
-//var tiles = JSON.parse("./tiles.json");
+}
+async function GetJson(url) {
+    return await fetch(url).then(r => r.json())
+                           .then(data => data)
+                           .catch(e => console.log("JSON couldn't be fetched. Error: " + e));
+}
 
 function getLocale(key){
     const regex = /\{.*\}/g;
-    s = en[key];
+    s = locale[key];
     while(regex.exec(s)){
         var matches = s.match(regex);
         for(var i in matches){
-            s = s.replace(i, locale[i.substring(1, i.length - 1)]);
+            console.log(matches[i]);
+            s = s.replace(matches[i], locale[matches[i].substring(1, matches[i].length - 1)]);
         }
     }
-
     return s;
 }
 
-console.log(getLocale("tile_solid_hidden_tip"));
 
-function createTable(res, width){
+function createTable(res, width, header){
     var table = document.createElement('table');
+    table.setAttribute("id", "category_" + header);
     var tableBody = document.createElement('tbody');
-  
+    // Ew, use a reduce.
+    //tableBody.appendChild(
+    //    document.createElement('tr').appendChild(
+    //        document.createElement('td').appendChild(
+    //            document.createElement('h3').appendChild(
+    //                document.createTextNode(header)))));
+
+
     for(let i = 0; i < Math.ceil(res.length / width); ++i){
         var row = document.createElement('tr');
         for(let j = 0; j < width; ++j){
-            var cell = document.createElement('td');
-            cell.setAttribute("id", "tile_" + (i * width + j));
             if(res.length < i * width + j + 1){
                 break;
             }
-            else{
-                cell.appendChild(document.createTextNode((i*width + j)));
-                row.appendChild(cell);
-            }
+            var tile = tiles[res[i *width + j]];
+            var id = tile.ids[0];
+            var cell = document.createElement('td');
+            cell.setAttribute("id", "tile_" + id);
+            
+            img = document.createElement("IMG");
+            img.setAttribute("src", "../assets/images/tiles/small/" + id + ".png");
+            img.setAttribute("alt", id);
+
+            cell.appendChild(img);
+            row.appendChild(cell);
+            
         }
         tableBody.appendChild(row);
     }
 
-    table.appendChild(tableBody);
-    document.getElementById("tile_table").appendChild(table);
+    //table.appendChild(tableBody);
+    document.getElementById("tile_table").appendChild(
+        document.createElement('h3').appendChild(
+            document.createTextNode(header)));
+    document.getElementById("tile_table").appendChild(tableBody);
 }
+
+Setup();
--- reboot/src/reference/tiles.md
+++ reboot/src/reference/tiles.md
@@ -5,4 +5,7 @@ Test
 <script src="https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.0/papaparse.min.js"></script>
 <script src="../assets/scripts/tiles.js"></script>
 
-<div id="tile_table"></div>
+<div id="tile_container">
+    <div id="tile_table"></div>
+    <div id="tile_data"></div>
+</div>
